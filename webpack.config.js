32a4e081d129f078536d617c9e7039be146fb49d
Steve Gentile <steven.gentile@gmail.com>
2015-12-29 14:49:37 -0500

    fixes to the config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
-var webpack = require('webpack'),
-	path = require('path'),
-	HtmlwebpackPlugin = require('html-webpack-plugin'),
-	merge = require('webpack-merge');
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
+var webpack = require('webpack');
 
 const TARGET = process.env.npm_lifecycle_event;
-
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
@@ -12,51 +11,52 @@ const PATHS = {
 
 process.env.BABEL_ENV = TARGET;
 
-var common = {
+const common = {
   entry: PATHS.app,
-  // Given webpack-dev-server runs in-memory, we can drop
-  // `output`. We'll look into it again once we get to the
-  // build chapter.
-  /*output: {
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
     path: PATHS.build,
     filename: 'bundle.js'
-  },*/
-  // Add resolve.extensions. '' is needed to allow imports an extension
-  // Note the .'s before extensions!!! Without those matching will fail
-  resolve:{
-	extensions: ['', '.js', '.jsx']
   },
   module: {
     loaders: [
       {
         test: /\.css$/,
-        loaders: ['babel', 'style', 'css'],
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
         include: PATHS.app
       }
     ]
   },
   plugins: [
-    // Important! move HotModuleReplacementPlugin below
-    //new webpack.HotModuleReplacementPlugin(),
     new HtmlwebpackPlugin({
-      title: 'Kanban app'
+      template: 'node_modules/html-webpack-template/index.html',
+      title: 'Kanban app',
+      appMountId: 'app'
     })
   ]
 };
 
-if(TARGET === 'start' || !TARGET) { //provides a default in case we're running Webpack outside of npm.
+if(TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
-    devtool: 'eval-source-map',  //setup sourcemaps
+    devtool: 'eval-source-map',
     devServer: {
       historyApiFallback: true,
       hot: true,
       inline: true,
       progress: true,
 
-      // Display only errors to reduce the amount of output.
+      // display only errors to reduce the amount of output
       stats: 'errors-only',
 
-      // Parse host and port from env so this is easy to customize.
+      // parse host and port from env so this is easy
+      // to customize
       host: process.env.HOST,
       port: process.env.PORT
     },
@@ -64,4 +64,8 @@ if(TARGET === 'start' || !TARGET) { //provides a default in case we're running W
       new webpack.HotModuleReplacementPlugin()
     ]
   });
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
 }
