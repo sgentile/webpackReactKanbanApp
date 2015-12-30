8a9bf6ce66e3c7f693007c1c98136abff68954dd
Steve Gentile <steven.gentile@gmail.com>
2015-12-30 14:33:58 -0500

    add a new note

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,21 +2,6 @@ import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn Webpack'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
-  }
-];
-
 /*
 * notes:
 * {} below allows mixing JS syntax within JSX.  map returns a list of li elements for React to render
@@ -24,9 +9,48 @@ const notes = [
 * */
 
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+            notes: [
+                {
+                    id: uuid.v4(),
+                    task: 'Learn Webpack'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Learn React'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Do laundry'
+                }
+            ]
+        };
+	}
 	render() {
+		const notes = this.state.notes;
 		return (
-			<Notes notes={notes} />
+            <div>
+                <button className="add-note" onClick={this.addNote}>+</button>
+			    <Notes notes={notes} />
+            </div>
 		);
 	}
+    
+    addNote = () => {
+        this.setState({
+           // this could make an async call to fetch a new id, etc... 
+           notes: this.state.notes.concat([{
+               id: uuid.v4(),
+               task: 'New Task'
+           }]) 
+        });
+        
+        /*
+        You could use [...this.state.notes, {id: uuid.v4(), task: 'New task'}] to achieve the same result. 
+        This spread operator can be used with function parameters as well.
+        */
+    }
 }
